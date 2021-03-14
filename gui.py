cec79ace9772820bb8816c3b6595d1d099641b35
Eric Chen <ezchen2556@gmail.com>
2021-03-14 00:50:16 -0800

    Read from clipboard and add to database

--- gui.py
+++ gui.py
@@ -2,28 +2,46 @@ from database import Database
 import tkinter as tk
 from datetime import datetime as dt
 
-BUTTON_WIDTH = 50
+class GUI:
 
-def copy_to_clipboard(text):
-    root.clipboard_clear()
-    root.clipboard_append(text)
+    def __init__(self):
+        self.width = 50
+        self.truncate_length = 60
+        self.delay = 100
+        self.root = tk.Tk()
+        self.db = Database()
+        self.prev_clip = self.root.clipboard_get()
+    def copy_to_clipboard(self, text):
+        self.root.clipboard_clear()
+        self.root.clipboard_append(text)
 
-def update_clipboard_history():
-    text = root.clipboard_get()
-    date_time = dt.today().strftime("%m-%d-%Y, %H:%M:%S")
-    db.store_clip(date_time, text)
+    def read_from_clipboard(self):
+        text = self.root.clipboard_get()
+        if text != self.prev_clip:
+            self.prev_clip = text
+            print("copied")
+            date_time = dt.today().strftime("(%m-%d %H:%M)")
+            self.db.store_clip(date_time, text)
+        
+        self.root.after(self.delay, self.read_from_clipboard)
+    
+    def run(self):
+        rows = self.db.get_clips()
+        for datetime, text in rows:
+            button_text = f"{datetime}: {text}"
+
+            if len(button_text) > self.truncate_length:
+                button_text = button_text[:self.truncate_length] + " ..."
+
+            button = tk.Button(self.root, width=self.width, text=button_text, command=lambda text=text: self.copy_to_clipboard(text))
+            button.pack(fill=tk.X, side=tk.BOTTOM)
+
+        clear_button = tk.Button(self.root, width=self.width // 5, text="Clear History", command=lambda: self.db.clear_clips())
+        clear_button.pack()
+        
+        self.read_from_clipboard()
+        self.root.mainloop()
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    db = Database()
-    db.store_clip("2", "aaaaa")
-    db.store_clip(3, "dadfasdf")
-
-    rows = db.get_clips()
-    for datetime, text in rows:
-        button_text = f"{datetime}: {text}"
-        b = tk.Button(root, width=BUTTON_WIDTH, text=button_text, command=lambda text=text: copy_to_clipboard(text))
-        b.pack(fill=tk.X, side=tk.BOTTOM)
-
-    root.mainloop()
-    #update_clipboard_history()
+    GUI().run()
+    

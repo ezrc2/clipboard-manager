697d841b3fe205737f5d63aff5bd163a4d389644
Eric Chen <ezchen2556@gmail.com>
2021-03-13 18:29:29 -0800

    Display each row of the table as a button

--- gui.py
+++ gui.py
@@ -1,21 +1,29 @@
+from database import Database
 import tkinter as tk
+from datetime import datetime as dt
 
-BUTTON_WIDTH = 40
-BUTTON_HEIGHT = 10
+BUTTON_WIDTH = 50
 
 def copy_to_clipboard(text):
-    window.clipboard_clear()
-    window.clipboard_append(text)
+    root.clipboard_clear()
+    root.clipboard_append(text)
 
-window = tk.Tk()
+def update_clipboard_history():
+    text = root.clipboard_get()
+    date_time = dt.today().strftime("%m-%d-%Y, %H:%M:%S")
+    db.store_clip(date_time, text)
 
-button = tk.Button(
-    text="button",
-    width=BUTTON_WIDTH,
-    bg="white",
-    fg="black",
-    command=copy_to_clipboard("dsdfadf")
-)
-button.pack()
+if __name__ == "__main__":
+    root = tk.Tk()
+    db = Database()
+    db.store_clip("2", "aaaaa")
+    db.store_clip(3, "dadfasdf")
 
-window.mainloop()
+    rows = db.get_clips()
+    for datetime, text in rows:
+        button_text = f"{datetime}: {text}"
+        b = tk.Button(root, width=BUTTON_WIDTH, text=button_text, command=lambda text=text: copy_to_clipboard(text))
+        b.pack(fill=tk.X, side=tk.BOTTOM)
+
+    root.mainloop()
+    #update_clipboard_history()
